8b34f889ce02dd1ee5903fdb1861685d5910217a
greenn-lab <hello9reen@gmail.com>
2019-01-21 12:17:51 +0900

    todo HTML scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="todoapp">
+    <header>
+      <h1>todo</h1>
+      <input type="text" class="new-todo" v-model="newTodo">
+    </header>
+
+    <section class="main">
+      <input id="toggle-all" class="toggle-all" type="checkbox">
+      <label for="toggle-all" title="Mark all as complete"></label>
+      <ul class="todo-list">
+        <li class="todo">
+          <!-- class in "completed", "editing" -->
+          <div class="view">
+            <input class="toggle" type="checkbox">
+            <label>할일을 적어봐요.</label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" type="text">
+        </li>
+      </ul>
+    </section>
+    <footer class="footer">
+      <span class="todo-count">
+        <strong>1 item</strong> left
+      </span>
+      <ul class="filters">
+        <li>
+          <a href="#/all" class="selected">All</a>
+        </li>
+        <li>
+          <a href="#/active">Active</a>
+          <!-- class in "selected" -->
+        </li>
+        <li>
+          <a href="#/completed">Completed</a>
+          <!-- class in "selected" -->
+        </li>
+      </ul>
+      <button class="clear-completed">Clear completed</button>
+    </footer>
+  </section>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import "todomvc-app-css/index.css";
 
 export default {
   name: "app",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [],
+      newTodo: ""
+    };
   }
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
